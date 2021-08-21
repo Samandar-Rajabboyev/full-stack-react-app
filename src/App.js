d98c28ce6581bedfdbe0b62dfad6c5c08336d6fe
◦•●◉ԲμҬμЯ૯ ૮ଇԁ૯Я◉●•◦ <81414536+7coder79@users.noreply.github.com>
2021-08-21 19:55:46 +0500

    finished full-stack project with reactjs

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if(list) {
+    return JSON.parse(localStorage.getItem("list"))
+  }else {
+    return []
+  }
+}
 
 function App() {
 
   const [name, setName] = useState('');
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null)
   const [alert, setAlert] = useState({show: false, msg: '', type: ''});
@@ -53,6 +61,10 @@ function App() {
     setEditId(id)
     setName(specificItem.title)
   }
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
 
   return (
     <section className='section-center'>
